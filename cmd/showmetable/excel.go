6ab0f62e644667a8a669ad0c029bb231a9fb4a59
bingoohuang <bingoo.huang@gmail.com>
2020-01-21 15:40:01 +0800

    adjust tmpl excel

--- cmd/showmetable/excel.go
+++ cmd/showmetable/excel.go
@@ -22,29 +22,25 @@ const sheet1 = "Sheet1"
 
 // WriteTable writes table information
 func (w *excelWriter) WriteTable(st model.Table) {
-	a := fmt.Sprintf("A%d", w.lineIndex)
 	b := fmt.Sprintf("B%d", w.lineIndex)
 	c := fmt.Sprintf("C%d", w.lineIndex)
-	d := fmt.Sprintf("D%d", w.lineIndex)
 	f := fmt.Sprintf("F%d", w.lineIndex)
 
-	_ = w.f.SetCellValue(sheet1, a, "表名")
-	_ = w.f.SetCellStyle(sheet1, a, a, w.getCellStyle("A1"))
-
-	_ = w.f.MergeCell(sheet1, b, c)
-	_ = w.f.SetCellValue(sheet1, b, st.GetName())
+	_ = w.f.SetCellValue(sheet1, b, "表名")
 	_ = w.f.SetCellStyle(sheet1, b, b, w.getCellStyle("B1"))
 
-	_ = w.f.MergeCell(sheet1, d, f)
-	_ = w.f.SetCellValue(sheet1, d, st.GetComment())
-	_ = w.f.SetCellStyle(sheet1, d, d, w.getCellStyle("D1"))
+	_ = w.f.SetCellValue(sheet1, c, st.GetName())
+	_ = w.f.SetCellStyle(sheet1, c, c, w.getCellStyle("C1"))
+
+	_ = w.f.SetCellValue(sheet1, f, st.GetComment())
+	_ = w.f.SetCellStyle(sheet1, f, f, w.getCellStyle("F1"))
 
 	w.lineIndex += 2
 }
 
 // WriteColumns writes columns of table
 func (w *excelWriter) WriteColumns(columns []model.TableColumn) {
-	w.writeHeadCells("A", "#", "字段名称", "数据类型", "是否可空", "默认值", "字段注释")
+	w.writeHeadCells("A", "#", "字段", "类型", "空", "默认", "注释")
 
 	w.lineIndex++
 
@@ -101,11 +97,7 @@ func (w *excelWriter) getCellStyle(axis string) int {
 
 func makeExcelWriter() *excelWriter {
 	tmplFs, _ := fs.New()
-	tmplXlsx := bytes.NewReader(tmplFs.Files["/tmpl.xlsx"].Data)
-	tmpl, _ := excelize.OpenReader(tmplXlsx)
-
-	_ = tmpl.SetSheetPrOptions(sheet1, excelize.FitToPage(true))
-	_ = tmpl.SetPageLayout(sheet1, excelize.FitToHeight(2), excelize.FitToWidth(2)) // nolint gomnd
+	tmpl, _ := excelize.OpenReader(bytes.NewReader(tmplFs.Files["/tmpl.xlsx"].Data))
 
 	return &excelWriter{
 		lineIndex: 1, // nolint gomnd
